98a7ce41aaec71cc7ed2e68e1c724c640d4ff520
nuzihr <naoya.ujihara@gmail.com>
2021-12-01 01:02:41 +0900

    fix dynamodb

--- main.go
+++ main.go
@@ -7,26 +7,12 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		_, err := dynamodb.NewTable(ctx, "money-log-table", &dynamodb.TableArgs{
+		tableName := "money-log-table"
+		_, err := dynamodb.NewTable(ctx, tableName, &dynamodb.TableArgs{
+			Name: pulumi.String(tableName),
 			Attributes: dynamodb.TableAttributeArray{
 				&dynamodb.TableAttributeArgs{
 					Name: pulumi.String("Title"),
-					Type: pulumi.String("S"),
-				},
-				&dynamodb.TableAttributeArgs{
-					Name: pulumi.String("JapaneseTitle"),
-					Type: pulumi.String("S"),
-				},
-				&dynamodb.TableAttributeArgs{
-					Name: pulumi.String("Price"),
-					Type: pulumi.String("N"),
-				},
-				&dynamodb.TableAttributeArgs{
-					Name: pulumi.String("PrimaryCategory"),
-					Type: pulumi.String("S"),
-				},
-				&dynamodb.TableAttributeArgs{
-					Name: pulumi.String("SecondaryCategory"),
 					Type: pulumi.String("S"),
 				},
 				&dynamodb.TableAttributeArgs{
@@ -34,10 +20,10 @@ func main() {
 					Type: pulumi.String("S"),
 				},
 			},
-			BillingMode: pulumi.String("PROVISIONED"),
-			HashKey:      pulumi.String("Title"),
-			RangeKey:     pulumi.String("Date"),
-			ReadCapacity: pulumi.Int(1),
+			BillingMode:   pulumi.String("PROVISIONED"),
+			HashKey:       pulumi.String("Title"),
+			RangeKey:      pulumi.String("Date"),
+			ReadCapacity:  pulumi.Int(1),
 			WriteCapacity: pulumi.Int(1),
 			Tags: pulumi.StringMap{
 				"Environment": pulumi.String("production"),
